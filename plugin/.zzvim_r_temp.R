090c92b88dcbc62a98559ef7c82b5484dc98ddf4
r. glenn thomas <rgthomas@ucsd.edu>
2025-11-27 10:00:56 -0800

    Auto-backup: 2025-11-27 10:00:56

--- plugin/.zzvim_r_temp.R
+++ plugin/.zzvim_r_temp.R
@@ -1,10 +1,1 @@
-penguins  %>%
-  filter(!is.na(bill_len), !is.na(bill_dep)) %>%
-  mutate(
-    bill_ratio = bill_len / bill_dep,
-    size_category = case_when(
-      body_mass < 3500 ~ "small",
-      body_mass < 4500 ~ "medium",
-      TRUE ~ "large"
-    )
-  )
+library(dplyr)
